1343cc699e405bea8c93d17f8238571ed0d2eade
Pl4cek <pl4cek@outlook.com>
2018-05-03 21:00:55 +0200

    applicationContext.xml -> dodanie konfiguracji Bean'ów BaseballCoach.java -> dodanie konstruktora, do którego wstrzykiwana jest fortuneService

--- src/main/java/dependencyInjection/BaseballCoach.java
+++ src/main/java/dependencyInjection/BaseballCoach.java
@@ -2,6 +2,12 @@ package dependencyInjection;
 
 public class BaseballCoach implements Coach{
 	
+	private FortuneService fortuneService;
+	
+	public BaseballCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Plan treningu dla baseballu";
